a5feee584be063c28464a01fa074e805a4aaeab7
DylanOakes <Y382Z945@wichita.edu>
2026-05-16 12:22:55 -0500

    Added links to other pages in base, and routes for adding assignments, querying assingments, and deleting users

--- app/routes.py
+++ app/routes.py
@@ -27,7 +27,7 @@ def add_users():
     if request.method == 'POST':
         user_id = request.form['user_id']
         username = request.form['username']
-        Email = request.form['email']
+        email = request.form['email']
         phone_num = request.form['phone_num']
         join_date = request.form['join_date']
     
@@ -56,6 +56,56 @@ def add_users():
         return redirect(url_for('main.users'))
     
     return render_template('add_users.html')
+
+@main.route('/users/delete/<string:user_id>')
+def delete_user(user_id):
+    user = users.query.get_or_404(user_id)
+    db.session.delete(user)
+    db.session.commit()
+    flash('User deleted!', 'warning')
+    return redirect(url_for('main.users'))
+
+@main.route('/assignments')
+def assignments():
+    data = a_status.query.all()
+    return render_template('assignments.html', assignments=data)
+
+@main.route('/assignments/add', methods=['GET', 'POST'])
+def add_assignment():
+    u_list = users.query.all()
+
+    if request.method == 'POST':
+        task_id = request.form['task_id']
+        user_id = request.form['user_id']
+        task_name = request.form['task_name']
+        task_desc = request.form['task_desc']
+        is_complete = request.form['is_complete']
+        due_date = request.form['due_date']
+
+        if not task_id.strip():
+            flash('Task ID is required.', 'error')
+            return redirect(url_for('main.add_assignment'))
+
+        if not task_name.strip():
+            flash('Task Name is required.', 'error')
+            return redirect(url_for('main.add_assignment'))
+        
+        try:
+            detail = a_details(TaskID=task_id, TaskName=task_name, TaskDesc=task_desc)
+            db.session.add(detail)
+            db.session.flush()
+
+            assignment_status = a_status(TaskID=task_id, UserID=user_id, isComplete=is_complete, DueDate=due_date)
+            db.session.add(assignment_status)
+            db.session.commit()
+
+            flash('Assignment added successfully', 'success')
+        
+        except Exception as e:
+            db.session.rollback()
+            flash(f'Error adding assignment: {str(e)}', 'error')
+        
+        return redirect(url_for('assignments.html'))
 
 ## @main.route("/")
 ## def index():
